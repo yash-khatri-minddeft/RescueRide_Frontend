bbaa10a980ca75bc72b82544c324629e8c1e355b
Yash Khatri <yash.khatri@minddeft.net>
2023-05-25 16:16:49 +0530

    admin signup

---
+++ public/pages/AdminSignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AdminSignUp() {
+  return (
+    <div className="login-page">
+      <div className="container">
+        
+      </div>
+    </div>
+  )
+}
