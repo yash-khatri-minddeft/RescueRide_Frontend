ce73a7b0500d2d67e0ba790296cbc78ee0c60798
Yash Khatri <yash.khatri@minddeft.net>
2023-05-26 16:16:11 +0530

    admin dashboard

---
+++ public/components/AddHospitalComponent.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+export default function AddHospitalComponent() {
+  useEffect(() => {
+    
+  },)
+  return (
+    <div className='admin-dashboard-inner'>
+      <h2 className='dashboard-header'>Add Hospital</h2>
+    </div>
+  )
+}
