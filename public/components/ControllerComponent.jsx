52e1a9d0f086d928bfaeaa7a5f8df361a3f9bd55
Yash Khatri <yash.khatri@minddeft.net>
2023-05-29 10:55:43 +0530

    empty controller list checker

--- public/components/ControllerComponent.jsx
+++ public/components/ControllerComponent.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import ControllerList from './ControllerList';
 
@@ -11,22 +11,24 @@ export default function ControllerComponent({ controllers, setModalShow }) {
         <h2 className='dashboard-header'>Controllers List</h2>
         <button onClick={setModalShow}>Add Controller</button>
       </div>
-      <div className="table-responsive">
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Controller Name</th>
-              <th>Controller Email</th>
-              <th>Controller PhoneNo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {controllers?.map((controller, key) => {
-              return (<ControllerList key={key} controller={controller} />)
-            })}
-          </tbody>
-        </table>
-      </div>
+      {controllers?.length ?
+        <div className="table-responsive">
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Controller Name</th>
+                <th>Controller Email</th>
+                <th>Controller PhoneNo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {controllers?.map((controller, key) => {
+                return (<ControllerList key={key} controller={controller} />)
+              })}
+            </tbody>
+          </table>
+        </div> : <>No controllers found. Please add some controllers from above button</>}
+
     </div>
   )
 }
