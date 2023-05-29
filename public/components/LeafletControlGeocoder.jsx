ad32df2cbc7ca4d27e5a16a3c95514b4672b485f
Yash Khatri <yash.khatri@minddeft.net>
2023-05-29 20:23:49 +0530

    addedd map and images

---
+++ public/components/LeafletControlGeocoder.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useMap } from "react-leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import L from "leaflet";
+import icon from "./mapConstants";
+
+export default function LeafletControlGeocoder({ setLatitude, setLongitude }) {
+  const map = useMap();
+
+  useEffect(() => {
+    return() => {
+      var geocoder = L.Control.Geocoder.nominatim();
+      if (typeof URLSearchParams !== "undefined" && location.search) {
+        // parse /?geocoder=nominatim from URL
+      var params = new URLSearchParams(location.search);
+      var geocoderString = params.get("geocoder");
+      if (geocoderString && L.Control.Geocoder[geocoderString]) {
+        geocoder = L.Control.Geocoder[geocoderString]();
+      } else if (geocoderString) {
+        console.warn("Unsupported geocoder", geocoderString);
+      }
+    }
+    L.Control.geocoder({
+      query: "",
+      placeholder: "Search here...",
+      defaultMarkGeocode: false,
+      geocoder
+    })
+      .on("markgeocode", function (e) {
+        var latlng = e.geocode.center;
+        setLatitude(latlng.lat)
+        setLatitude(latlng.lng)
+        L.marker(latlng, { icon })
+          .addTo(map)
+          .bindPopup(e.geocode.name)
+          .openPopup();
+        map.fitBounds(e.geocode.bbox);
+      })
+      .addTo(map);
+    }
+  }, []);
+  
+  return null;
+}
