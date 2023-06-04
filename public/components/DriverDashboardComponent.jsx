4f5a4aa6a5fd5339218936207183e7aa143572a8
ParthMinddeft <parth.talsaniya@minddeft.net>
2023-06-04 18:04:55 +0530

    driverbook

--- public/components/DriverDashboardComponent.jsx
+++ public/components/DriverDashboardComponent.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function DriverDashboardComponent() {
+export default function DriverDashboardComponent({ bookingDriver }) {
   return (
     <div className="admin-dashboard-inner">
       <div className="admin-dashboard-upper">
         <h2 className="dashboard-header">Booking Requests</h2>
       </div>
       <div className="table-responsive">
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+        {bookingDriver?.length ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Phone No.</th>
+                <th>Hospital Name</th>
+                <th>ReachedDestination</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookingDriver?.map((booking, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{booking.booking.username}</td>
+                    <td>
+                      <a href={`tel:${booking.booking.number}`}>
+                        {booking.booking.number}
+                      </a>
+                    </td>
+                    <td>{booking.hospital.address}</td>
+                    <td>
+                      <Link
+                        className="button"
+                        to={`/booking-request/${booking.driver._id}`}
+                      >
+                        <i className="fa-location-dot"></i>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <>No Driver Bookings</>
+        )}
       </div>
     </div>
-  )
+  );
 }
