36c2d69701f3e8969d5c7f5b80a4ff67f987c158
Yash Khatri <yash.khatri@minddeft.net>
2023-06-06 15:47:27 +0530

    changed proxy setting

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://api-rescueride.onrender.com',
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        secure: false
+      }
     }
   }
 })
