bff6a276bb52de5ba8c1cf5df95e9ac1206e5f83
Yash Khatri <yash.khatri@minddeft.net>
2023-06-08 12:02:34 +0530

    map zoom

--- public/components/AddHospital.jsx
+++ public/components/AddHospital.jsx
@@ -13,7 +13,7 @@ import LeafletControlGeocoder from "./LeafletControlGeocoder";
 const RecenterAutomatically = ({ lat, lng }) => {
   const map = useMap();
   useEffect(() => {
-    map.setView([lat, lng], 11, {
+    map.setView([lat, lng], 12, {
       animate: true
     });
   }, [lat, lng]);
