abbf9705f48d16dcc4d2641086f2b361d7f49ab8
Yash Khatri <yash.khatri@minddeft.net>
2023-06-09 12:08:54 +0530

    added fixed decimal point in hospital table

--- public/components/HospitalList.jsx
+++ public/components/HospitalList.jsx
@@ -4,8 +4,8 @@ export default function HospitalList({hospital}) {
     return(
         <tr>
             <td>{hospital.address}</td>
-            <td>{hospital.longitude}</td>
-            <td>{hospital.latitude}</td>
+            <td>{hospital.longitude.toFixed(7)}</td>
+            <td>{hospital.latitude.toFixed(7)}</td>
         </tr>
     )
 }
